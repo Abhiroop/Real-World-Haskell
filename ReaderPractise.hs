c8ffe04f6da76b7c34de64b73b30dce6a8ec9961
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-02-25 04:33:09 +0530

    Type signature

--- ReaderPractise.hs
+++ ReaderPractise.hs
@@ -48,6 +48,7 @@ bolt = liftA2 (&&) (>3) (<8)
 sequA :: Integral a => a -> [Bool]
 sequA m = sequenceA [(>3), (<8), even] m
 
+s' :: Maybe Integer
 s' = summed <$> ((,) <$> xs <*> ys)
 
 main :: IO ()
