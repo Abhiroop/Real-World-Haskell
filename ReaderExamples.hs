29c91095278810fc5a01c972c18376441d81b5ed
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-02-25 01:08:43 +0530

    Add monad instance and what not

--- ReaderExamples.hs
+++ ReaderExamples.hs
@@ -109,5 +109,34 @@ instance Applicative (Reader r) where
     -- Reader $ \r -> rab r (ra r)
 
  ------------------------------------------------------------
+foo :: (Functor f, Num a) => f a -> f a
+foo r = fmap (+1) r
 
-  
+bar :: Foldable f => t -> f a -> (t, Int)
+bar r t = (r,length t)
+
+barOne :: Foldable t => t a -> (t a, Int)
+barOne r = (r,length r)
+
+barPlus :: (Functor t, Num a, Foldable t) => t a -> (t a, Int)
+barPlus r = (foo r, length r)
+
+frooty :: Num a => [a] -> ([a], Int)
+frooty = \r -> bar (foo r) r
+
+fooBind :: (r -> a) -> (a -> r -> b) -> r -> b
+fooBind m k = \r -> k (m r) r
+
+getDogRM :: Person -> Dog
+getDogRM = do
+  name <- dogName
+  addy <- address
+  return $ Dog name addy
+
+----------------------------------------------------------
+
+instance Monad (Reader r) where
+  return = pure
+
+  (>>=) :: Reader r a ->(a->Reader r b) -> Reader r b
+  (Reader ra) >>= aRb = Reader $ \r -> (runReader $ aRb (ra r)) r
