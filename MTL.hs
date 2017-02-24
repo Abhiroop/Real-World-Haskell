cf5831453b07dffa5874c81ee47f4fe60ae14aa2
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-02-25 02:48:54 +0530

    The Reader Transformer

--- MTL.hs
+++ MTL.hs
@@ -75,3 +75,31 @@ instance (Monad m) => Monad (MaybeT m) where
        Nothing -> return Nothing
        Just y  -> runMaybeT (f y)
 ----------------------------------------------------------------------------------
+
+-- EitherT
+
+---------------------------------------------------------------------------------
+
+newtype ReaderT r m a =
+  ReaderT { runReaderT :: r -> m a }
+
+instance (Functor m) => Functor (ReaderT r m) where
+  fmap f (ReaderT rma) =
+    ReaderT $ (fmap.fmap) f rma
+
+instance (Applicative m) => Applicative (ReaderT r m) where
+
+  pure a = ReaderT (pure (pure a))
+
+  (ReaderT fmab) <*> (ReaderT rma) = ReaderT $ liftA2 (<*>) fmab rma
+
+instance (Monad m) => Monad (ReaderT r m) where
+
+  return = pure
+
+  (>>=) :: ReaderT r m a -> (a -> ReaderT r m b) -> ReaderT r m b
+  (ReaderT rma) >>= f =
+    ReaderT $ \r -> do
+      a <- rma r
+      runReaderT (f a) r
+-------------------------------------------------------------------------------
