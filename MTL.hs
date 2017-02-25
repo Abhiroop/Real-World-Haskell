58f000e8514bc34813432ea9cd8df2e9915b84b6
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-02-25 18:04:28 +0530

    Add the functor instance of StateT

--- MTL.hs
+++ MTL.hs
@@ -3,6 +3,7 @@
 module MTL where
 
 import Control.Applicative
+import Data.Bifunctor
 
 newtype Identity a = Identity {runIdentity :: a} deriving (Eq,Show)
 
@@ -103,3 +104,13 @@ instance (Monad m) => Monad (ReaderT r m) where
       a <- rma r
       runReaderT (f a) r
 -------------------------------------------------------------------------------
+
+newtype StateT s m a =
+  StateT { runStateT :: s -> m (a,s) }
+
+instance (Functor m) => Functor (StateT s m) where
+
+  fmap :: (a -> b)-> StateT s m a -> StateT s m b
+  fmap f (StateT sma) = StateT $
+    \s -> let ma = sma s -- ma :: m (a,s)
+          in  fmap (first f) ma -- Eg: first (+3) (1,2) = (4,2) first is from Bifunctor
