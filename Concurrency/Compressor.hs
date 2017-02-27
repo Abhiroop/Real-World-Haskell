49b2b072d992653a2b3822a22cbce9a11ad90e0f
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-02-27 22:15:43 +0530

    Add the multi threded compressor

---
+++ Concurrency/Compressor.hs
@@ -0,0 +1,22 @@
+module Compresoor where
+
+import Control.Concurrent(forkIO)
+import Control.Exception (handle)
+import Control.Monad (forever)
+import qualified Data.ByteString.Lazy as L
+import System.Console.Readline (readline)
+
+import Codec.Compression.GZip (compress)
+
+main = do
+  maybeLine <- readline "Enter a file to compress"
+  case maybeLine of
+    Nothing -> return () -- user enterd EOF
+    Just "" -> return () -- treat no name as "want to quit"
+    Just name -> do
+      handle ((\e -> print e) :: IOError -> IO ()) $ do
+        content <- L.readFile name
+        forkIO (compressFile name content)
+        return ()
+      main
+ where compressFile path = L.writeFile (path ++ ".gz") . compress
