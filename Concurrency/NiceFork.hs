76a80d8cd407cc81045fdcaf27b338b98af48d17
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-02-28 18:28:31 +0530

    Manged for example

--- Concurrency/NiceFork.hs
+++ Concurrency/NiceFork.hs
@@ -1,4 +1,5 @@
-module NiceFork(ThreadManager, newManager, forkManaged, getStatus, waitFor, waitAll) where
+module NiceFork(ThreadManager, newManager --forkManaged, getStatus, waitFor, waitAll
+               ) where
 
 import Control.Concurrent
 import Control.Exception (Exception, try)
@@ -10,21 +11,31 @@ instance Exception ThreadException
 
 data ThreadStatus = Running | Finished | Threw ThreadException deriving (Eq,Show)
 
+newtype ThreadManager = Mgr (MVar (M.Map ThreadId (MVar ThreadStatus))) deriving Eq
+
 -- creates a new Thread Manager
 newManager :: IO ThreadManager
+newManager = do
+  mvar <- newMVar M.empty
+  return $ Mgr mvar
 
 -- creates a new managed thread
-forkManged :: ThreadManger -> IO () -> IO ThreadId
+forkManged :: ThreadManager -> IO () -> IO ThreadId
+forkManged (Mgr mgr) body =
+--modifyMVar :: MVar a -> (a -> IO (a, b)) -> IO b
+  modifyMVar mgr $ \m -> do
+    state <- newEmptyMVar
+    tid <- forkIO $ do
+      result <- try body
+      putMVar state (either Threw (const Finished) result)
+    return (M.insert tid state m, tid)
 
 -- immediately return the status of the thread
-getStatus :: ThreadManager -> ThreadId -> IO (Maybe ThreadStatus)
+--getStatus :: ThreadManager -> ThreadId -> IO (Maybe ThreadStatus)
 
--- block until a specific manged thread terminates
-waitFor :: Threadmanager -> ThreadId -> IO (Maybe ThreadStatus)
+-- -- block until a specific manged thread terminates
+-- waitFor :: Threadmanager -> ThreadId -> IO (Maybe ThreadStatus)
 
--- block until all manged threads terminate
-waitAll :: ThreadManger -> IO()
-
-
-
+-- -- block until all manged threads terminate
+-- waitAll :: ThreadManger -> IO()
 
