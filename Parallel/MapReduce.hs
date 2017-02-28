e341f003a973bda93410fd90412c5adf3ec6cc9b
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-03-01 03:23:44 +0530

    mapreduce

--- Parallel/MapReduce.hs
+++ Parallel/MapReduce.hs
@@ -1,3 +1,10 @@
 module MapReduce where
 
-f 1 = 2
+import Control.Parallel
+import Control.Parallel.Strategies
+
+mapReduce :: Strategy b -> (a -> b) -> Strategy c -> ([b] -> c) -> [a] -> c
+mapReduce mapStrat mapFunc reduceStrat reduceFunc input =
+  mapResult `pseq` reduceResult
+  where mapResult = parMap mapStrat mapFunc input
+        reduceResult = reduceFunc mapResult `using` reduceStrat
