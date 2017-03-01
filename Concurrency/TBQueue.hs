15bd2a17b17bcf58224aad3261c74d348f597728
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-03-02 00:19:41 +0530

    Create function for bounded queue

--- Concurrency/TBQueue.hs
+++ Concurrency/TBQueue.hs
@@ -1,2 +1,12 @@
 module TBQueue where
 
+import Control.Concurrent.STM hiding (TBQueue)
+
+data TBQueue a = TBQueue (TVar Int) (TVar [a]) (TVar [a])
+
+newTBQueue :: Int -> STM (TBQueue a)
+newTBQueue size = do
+  read <- newTVar []
+  write <- newTVar []
+  cap <- newTVar size
+  return (TBQueue cap read write)
