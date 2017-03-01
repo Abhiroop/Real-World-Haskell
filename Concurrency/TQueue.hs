6e1c47e9a0a46c409754bdc2f1ed7ed3af3a4bfa
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-03-01 22:35:15 +0530

    Add init for transactional queue

--- Concurrency/TQueue.hs
+++ Concurrency/TQueue.hs
@@ -1,5 +1,11 @@
 module TQueue where
 
-import Control.Concurrent.STM
+import Control.Concurrent.STM hiding (TQueue)
 
 data TQueue a = TQueue (TVar [a]) (TVar [a])
+
+newTQueue :: STM (TQueue a)
+newTQueue = do
+  read <- newTVar []
+  write <- newTVar []
+  return (TQueue read write)
