2b87bb842b3a16b33d15214aca3936eca0761888
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-03-02 00:23:08 +0530

    Add the write function for bounded chan

--- Concurrency/TBQueue.hs
+++ Concurrency/TBQueue.hs
@@ -10,3 +10,12 @@ newTBQueue size = do
   write <- newTVar []
   cap <- newTVar size
   return (TBQueue cap read write)
+
+writeTBQueue :: TBQueue a -> a -> STM ()
+writeTBQueue (TBQueue cap _ write) a = do
+  avail <- readTVar cap
+  if avail ==0
+    then retry --block here
+    else writeTVar cap (avail - 1)
+  listend <- readTVar write
+  writeTVar write (a:listend)
