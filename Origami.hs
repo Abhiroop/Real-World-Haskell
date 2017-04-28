653292a23109b8f735dece1cc7a76ac34eecf602
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-04-28 23:21:31 +0530

    Fix point combinator

--- Origami.hs
+++ Origami.hs
@@ -1,5 +1,10 @@
+{- LANGUAGE OverloadedStrings -}
 module Origami where
 
+import Data.Function(fix)
+import qualified Data.Map.Strict as M hiding (map)
+import Data.Maybe (fromJust)
+import Data.Graph
 data Fix s a = FixT {getFix :: s a (Fix s a)}
 
 data List_ a r = Nil | Cons_ a r
@@ -10,3 +15,30 @@ data Tree_ a r = Leaf_ a | Node_ a r r
 
 type ListF a = Fix List_ a
 type TreeF a = Fix Tree_ a
+
+data Info = Info {name :: String,parent :: [String]}
+
+
+type ClassName = String
+type Parent = String
+
+parseClassHeader :: ClassName -> [Parent]
+parseClassHeader c = M.findWithDefault [] c $ M.fromList [("D",["B"]),("B",["A"]),("E",["B"]),("F",["C"]),("G",["C"]),("C",["A"])]
+
+fixPointFunc :: [(ClassName,[Parent])] -> ClassName -> [(ClassName,[Parent])]
+fixPointFunc x c =
+  let parent = parseClassHeader c
+   in if null parent
+         then x
+         else let j = x ++ [(c,parent)]
+               in concatMap (fixPointFunc j) parent
+
+fixPointFunc' :: [(ClassName,[Parent])] -> ClassName -> [(ClassName,[Parent])]
+fixPointFunc' =
+  fix $ \ recur x c ->  let parent = parseClassHeader c
+                         in if null parent
+                             then x
+                             else let j = x ++ [(c,parent)]
+                                   in concatMap (recur j) parent
+
+
