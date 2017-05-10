d60f91865488e4f380d0407b5191ede3b7084a58
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-05-10 18:47:18 +0530

    Experimenting with lob etc

--- Origami.hs
+++ Origami.hs
@@ -1,10 +1,14 @@
 {- LANGUAGE OverloadedStrings -}
 module Origami where
-
+import Text.Regex.Posix.Wrap
 import Data.Function(fix)
 import qualified Data.Map.Strict as M hiding (map)
 import Data.Maybe (fromJust)
 import Data.Graph
+import System.IO.Unsafe
+import Foreign.C.String
+import Data.Either
+import Control.Applicative
 data Fix s a = FixT {getFix :: s a (Fix s a)}
 
 data List_ a r = Nil | Cons_ a r
@@ -41,4 +45,32 @@ fixPointFunc' =
                              else let j = x ++ [(c,parent)]
                                    in concatMap (recur j) parent
 
+loeb :: (Functor f) => f (f a -> a) -> f a
+loeb x = fmap (\ a -> a (loeb x)) x
+-- loeb x = go where go = fmap ($ go) x
+
+
+
+factorial n = fac n 1
+
+fac 1 acc = acc
+fac n acc = fac (n-1) (acc*n)
+
+
+f :: (IO a, IO a) -> IO (a, a)
+f (x,y)= do
+  a <- x
+  b <- y
+  return (a,b)
+
+f' :: (IO a, IO a) -> IO (a,a)
+f' = uncurry $ liftA2 (,)
+
+
+g :: IO ()
+g = do
+  x <- getLine
+  case (length x) of
+    0 -> putStrLn "Hello"
+    _ -> putStrLn "World"
 
