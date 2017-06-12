fb8fd721faf216aa31d441ed6756f4d2c3cad013
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-06-13 01:43:12 +0530

    Hand written generic instances

--- Generics.hs
+++ Generics.hs
@@ -26,4 +26,67 @@ instance Serializable a => Serializable (Tree a) where
     (root,bits'') = get bits'
     (right,bits''') = get bits''
 
+-- class Generic a where
 
+--   type Rep a :: * -> *
+
+--   from :: a -> (Rep a) p
+
+--   to :: (Rep a) p -> a
+
+data D1_Tree
+data C1_Tree_Leaf
+data C1_Tree_Branch
+
+instance Generic (Tree a) where
+
+  type Rep (Tree a) =
+        M1 D D1_Tree (
+            M1 C C1_Tree_Leaf U1
+            :+:
+            M1 C C1_Tree_Branch (
+                M1 S NoSelector (K1 R (Tree a))
+                :*:
+                M1 S NoSelector (K1 R a)
+                :*:
+                M1 S NoSelector (K1 R (Tree a))
+            )
+        )
+
+  from Leaf                     = M1 (L1 (M1 U1))
+  from (Branch left root right) = M1 (
+                                        R1 (
+                                        M1 (
+                                            M1 (K1 left)
+                                            :*:
+                                            M1 (K1 root)
+                                            :*:
+                                            M1 (K1 right)
+                                        ))
+                                    )
+
+  to (M1 (L1 (M1 U1)))      = Leaf
+  to (M1 (
+            R1 (
+            M1 (
+                M1 (K1 left)
+                :*:
+                M1 (K1 root)
+                :*:
+                M1 (K1 right)
+            ))
+        ))                    = Branch left root right
+
+instance Datatype D1_Tree where
+
+  datatypeName _ = "Tree"
+
+  moduleName _ = "Main"
+
+instance Constructor C1_Tree_Leaf where
+
+  conName _ = "Leaf"
+
+instance Constructor C1_Tree_Branch where
+
+  conName _ = "Branch"
