a1bb8e74c2b957d9604d3fd2cfdeeae5ca23f91c
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-07-16 00:34:34 +0530

    Catamorphism and anamorphism

---
+++ RecursionSchemes2.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveFunctor #-}
+module RecusrsionSchemes2 where
+
+import Control.Arrow
+import Data.Monoid
+import Text.PrettyPrint (Doc)
+import qualified Text.PrettyPrint as P
+
+data Expr a
+  = Literal { intVal :: Int }
+  | Ident   { name :: String  }
+  | Index   { target :: a, idx :: a }
+  | Unary   { op :: String, target :: a }
+  | Binary  { lhs :: a, op :: String, rhs :: a }
+  | Call    { func :: a, args :: [a] }
+  | Paren   { target :: a }
+  deriving (Show, Eq, Functor)
+
+type Algebra f a = f a -> a
+
+newtype Term f = In { out :: f (Term f) }
+
+ten, add, call :: Term Expr
+ten  = In (Literal { intVal = 10 })
+add  = In (Ident { name = "add" })
+call = In (Call { func = add, args = [ten, ten]}) -- add(10, 10)
+
+cata :: (Functor f) => Algebra f a -> Term f -> a
+cata f = out >>> fmap (cata f) >>> f
+
+ana :: (Functor f) => CoAlgebra f a -> a -> Term f
+ana f = In <<< fmap (ana f) <<< f
+
+countNodes :: Algebra Expr Int
+countNodes (Literal _) = 1
+countNodes (Ident   _) = 1
+countNodes (Unary _ arg)         = arg + 1
+countNodes (Binary left _ right) = left + right + 1
+countNodes (Call fn args)        = fn + sum args + 1
+countNodes (Index it idx)        = it + idx + 1
+countNodes (Paren arg)           = arg + 1
+
+prettyPrint :: Algebra Expr Doc
+prettyPrint (Literal i) = P.int i
+prettyPrint (Ident s) = P.text s
+prettyPrint (Call f as)     = f <> P.parens (P.hcat (P.punctuate "," as))   -- f(a,b...)
+prettyPrint (Index it idx)  = it <> P.brackets idx                 -- a[b]
+prettyPrint (Unary op it)   = (P.text op) <> it                    -- op x
+prettyPrint (Binary l op r) = l <> (P.text op) <> r                -- lhs op rhs
+prettyPrint (Paren exp)     = P.parens exp                         -- (op)
+
+bottomUp :: Functor a => (Term a -> Term a) -> Term a -> Term a
+bottomUp f = cata (In >>> f)
+
+type CoAlgebra f a = a -> f a
+
+
