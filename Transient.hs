f5c09ee07994ee66ddfe193cb94d26a99c75c35b
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-08-18 23:50:59 +0530

    Add monad instance and few basic functions

--- Transient.hs
+++ Transient.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ExistentialQuantification #-}
 module Transient where
 
@@ -5,15 +8,31 @@ import Control.Monad (liftM, ap)
 import Control.Monad.State.Class
 
 data EventF = forall m b c. EventF (b -> Transient m c)
+
 data Transient m x = Transient (m (Maybe x))
 
--- instance Functor (Transient m) where
---   fmap = liftM
+runTrans :: Transient m x -> m (Maybe x)
+runTrans (Transient mx) = mx
 
--- instance Applicative (Transient m) where
---   pure  = return
---   (<*>) = ap
+setEventCont :: MonadState EventF m => (b -> Transient m c) -> m EventF
+setEventCont f = do
+    f' <- get
+    put $ EventF f
+    return f'
 
--- instance MonadState m => Monad (Transient m) where
---   return x = Transient $ return $ Just x
---   x >>= f = undefined
+instance MonadState EventF m => Functor (Transient m) where
+  fmap = liftM
+
+instance MonadState EventF m => Applicative (Transient m) where
+  pure  = return
+  (<*>) = ap
+
+instance MonadState EventF m => Monad (Transient m) where
+  return x = Transient $ return $ Just x
+
+  x >>= f = Transient $ do
+    setEventCont f
+    mk <- runTrans x
+    case mk of
+      Just k -> runTrans (f k)
+      Nothing -> return Nothing
