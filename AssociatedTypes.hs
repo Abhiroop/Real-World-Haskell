971919854153971518059b8970830fb4ea241589
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-12-19 20:48:26 +0000

    Add type families to simplify

--- AssociatedTypes.hs
+++ AssociatedTypes.hs
@@ -4,19 +4,29 @@
 module AssociatedTypes where
 
 import Control.Concurrent.MVar
+import Control.Concurrent.STM
 import Data.IORef
 
-class IOStore store where
-  newIO :: a -> IO (store a)
-  getIO :: store a -> IO a
-  putIO :: store a -> a -> IO ()
+class Store store where
+  type StoreMonad store :: * -> *
+  new :: a -> (StoreMonad store) (store a)
+  get :: store a -> (StoreMonad store) a
+  put :: store a -> a -> (StoreMonad store) ()
 
-instance IOStore MVar where
-  newIO = newMVar
-  getIO = readMVar
-  putIO mvar a = modifyMVar_ mvar (return . const a)
+instance Store MVar where
+  type StoreMonad MVar = IO
+  new = newMVar
+  get = readMVar
+  put mvar a = modifyMVar_ mvar (return . const a)
 
-instance IOStore IORef where
-  newIO = newIORef
-  getIO = readIORef
-  putIO ioref a = modifyIORef ioref (const a)
+instance Store IORef where
+  type StoreMonad IORef = IO
+  new = newIORef
+  get = readIORef
+  put ioref a = modifyIORef ioref (const a)
+
+instance Store TVar where
+  type StoreMonad TVar = STM
+  new = newTVar
+  get = readTVar
+  put ioref a = modifyTVar ioref (const a)
