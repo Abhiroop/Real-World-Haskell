9d5f5aa7c0389f772bbdca6cd952ed18a08c54d2
Abhiroop Sarkar <asiamgenius@gmail.com>
2018-02-08 22:54:16 +0000

    Change in type?

--- LeafNumbering.hs
+++ LeafNumbering.hs
@@ -4,12 +4,12 @@ import Prelude hiding(traverse)
 
 -- Give a Tree number the leaves in increasing order.
 
-data Tree = Leaf Int | Node Int Tree Tree deriving Show
+data Tree = Leaf Int | Node (Int,Int) Tree Tree deriving Show
 
 leaves :: Tree -> Int
 leaves (Leaf l) = l
-leaves (Node x _ _) = x
+leaves (Node (x,y) _ _) = x + y
 
 traverse :: Tree -> Tree
 traverse (Leaf l) = (Leaf l)
-traverse (Node n x y) = Node ((leaves x) + (leaves y)) (traverse x) (traverse y)
+traverse (Node n x y) = Node ((leaves x),(leaves y)) (traverse x) (traverse y)
